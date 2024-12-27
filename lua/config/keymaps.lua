06c6f3e4a1d266efe25e8fca92c3296950acd47f
Sam Waters <sam.waters@payground.com>
2024-12-27 01:57:53 -0600

    remove some unused configs

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,9 +1,16 @@
+-- TODO
 local map = vim.keymap.set
 
--- Markdown
-map({ "n", "v" }, "<leader>cx", function()
-  return require("obsidian").util.toggle_checkbox()
-end, { desc = "Mark checkbox as done" })
--- map({ "n", "i", "v" }, "<leader>co", function()
---   return require("obsidian").util.()
+-- Examples:
+
+-- Terminal
+-- map({ "n" }, "<leader>;", ":lua NTGlobal['terminal']:toggle()<cr>", { desc = "Open integrated terminal" })
+--
+-- -- Markdown
+-- -- TODO: re-enable for insert mode, only for Markdown files
+-- map({ "n", "v" }, "<leader>cx", function()
+--   return require("obsidian").util.toggle_checkbox()
 -- end, { desc = "Mark checkbox as done" })
+-- -- map({ "n", "i", "v" }, "<leader>co", function()
+-- --   return require("obsidian").util.()
+-- -- end, { desc = "Mark checkbox as done" })
--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -7,17 +7,4 @@ return {
     "folke/todo-comments.nvim",
     enabled = false,
   },
-  {
-    "crusj/bookmarks.nvim",
-    enabled = false,
-    keys = {
-      { "<tab><tab>", mode = { "n" } },
-    },
-    branch = "main",
-    dependencies = { "nvim-web-devicons" },
-    config = function()
-      require("bookmarks").setup()
-      require("telescope").load_extension("bookmarks")
-    end,
-  },
 }
