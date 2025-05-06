43bbc93f94e5628152b9f0eee280d7afd6ebaa1a
Sam Waters <sam.waters@payground.com>
2025-05-05 19:01:11 -0700

    add lazy back

--- init.lua
+++ init.lua
@@ -1,8 +1,52 @@
--- Basic settings
 vim.opt.number = true -- Show line numbers
 vim.opt.relativenumber = true -- Enable relative line numbers
-vim.opt.tabstop = 2 -- Tab width
-vim.opt.shiftwidth = 2 -- Indentation width
 vim.opt.expandtab = true -- Use spaces instead of tabs
 vim.opt.wrap = false -- Disable line wrapping
-vim.opt.clipboard = "unnamedplus" -- Use the system clipboard for all yank, delete, change, and put operations
+vim.g.have_nerd_font = true
+
+vim.g.mapleader = "<Space>"
+vim.g.maplocalleader = "<Space>"
+
+-- Sync clipboard between OS and Neovim.
+--  Schedule the setting after `UiEnter` because it can increase startup-time.
+--  Remove this option if you want your OS clipboard to remain independent.
+--  See `:help 'clipboard'`
+vim.schedule(function()
+  vim.opt.clipboard = "unnamedplus"
+end)
+
+-- Save undo history
+vim.opt.undofile = true
+
+-- Case-insensitive searching UNLESS \C or one or more capital letters in the search term
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+-- Highlight when yanking (copying) text
+vim.api.nvim_create_autocmd("TextYankPost", {
+  desc = "Highlight when yanking (copying) text",
+  group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
+
+-- [[ Install `lazy.nvim` plugin manager ]]
+--    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  if vim.v.shell_error ~= 0 then
+    error("Error cloning lazy.nvim:\n" .. out)
+  end
+end ---@diagnostic disable-next-line: undefined-field
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({
+  {
+    "folke/which-key.nvim",
+    event = "VimEnter", -- Sets the loading event to 'VimEnter'
+    opts = {},
+  },
+})
