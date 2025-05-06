176029ffc28a93747600afff67aa5d5f31e79fb8
Sam Waters <sam.waters@payground.com>
2025-05-05 17:03:24 -0700

    remove lazy until needed

--- init.lua
+++ init.lua
@@ -5,25 +5,4 @@ vim.opt.tabstop = 2 -- Tab width
 vim.opt.shiftwidth = 2 -- Indentation width
 vim.opt.expandtab = true -- Use spaces instead of tabs
 vim.opt.wrap = false -- Disable line wrapping
-
--- Keymaps
-local map = vim.keymap.set
-
--- Bootstrap lazy.nvim if not installed
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable",
-    lazypath,
-  })
-end
-vim.opt.rtp:prepend(lazypath)
-
--- Plugin setup using lazy.nvim
-require("lazy").setup({
-  {},
-})
+vim.opt.clipboard = "unnamedplus" -- Use the system clipboard for all yank, delete, change, and put operations
